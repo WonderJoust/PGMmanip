969307abdb170003812faf43ac5a0ad1085836ef
WonderJoust <arthur.bousquet@gmail.com>
2013-10-25 13:36:07 -0500

    Create somefile.cpp
    This better get sync'd. Or else, Git.

---
+++ somefile.cpp
@@ -0,0 +1,16 @@
+TAG to do
+
+	Website
+	request - http://www.ttu.edu/accounts/web/index.php - www.ttu.edu/accounts/web/new.php?t=org
+
+		forums
+		forms
+		info
+			gameslist
+			discount info
+			current funds
+	
+	Logo
+		-tshirt?
+		-dues?
+		-flyers?
