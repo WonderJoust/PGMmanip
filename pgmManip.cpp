c70272721c586a890bbdb73626d71bf901aefc1f
WonderJoust <arthur.bousquet@gmail.com>
2013-10-27 23:35:18 -0500

    Parse successful.

--- pgmManip.cpp
+++ pgmManip.cpp
@@ -7,24 +7,49 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
-int pgmRead (string somefile, int pixels[]);
-
+int pgmRead (string somefile, string &tag, string header[], int pixels[][1024]);
 
 int main ()
 {
-	int pixels[1024*1024]={0};
+	string tag;
+	string header[3]={"0"};
+	int pixels[1024][1024];
 
-	string thisFile;
+	string thisFile = "somepic.pgm";
 
-	pgmRead (thisFile, pixels);
+	pgmRead (thisFile, tag, header, pixels);
 	return 0;
 }
 
 
-int pgmRead (string somefile, int pixels[])
+int pgmRead (string somefile, string &tag, string header[], int pixels[][1024])
 {
+	string thisLine;
+
+	ifstream inFile (somefile.c_str());
+
+	int j = 0;
+
+	while ( getline(inFile, thisLine) ) //pull line
+	{
+		cout << "The line is: " << thisLine << endl;
+
+		istringstream ss(thisLine);
+		//load in string parser
+		unsigned int i=0;
+		int x=0;
+		while (ss.good() && i < thisLine.length())
+		{
+			ss >> x;
+			cout << x<<endl;
+			i++;
+		}
+	}
+
 	return 0;
 }
